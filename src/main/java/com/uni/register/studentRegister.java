3e6ef3314d14983c88b97b852bd308ebc2ec01d6
Zero Ex <0xb4dc0d3x@gmail.com>
2023-02-03 23:40:08 +0330

    initial commit

--- src/main/java/com/uni/register/studentRegister.java
+++ src/main/java/com/uni/register/studentRegister.java
@@ -3,10 +3,11 @@ package com.uni.register;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.uni.register")
 public class studentRegister {
 
     public static void main(String[] args) {
+        System.setProperty("spring.profiles.active", "register");
         SpringApplication.run(studentRegister.class, args);
     }
 
