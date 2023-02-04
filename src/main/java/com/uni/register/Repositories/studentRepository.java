eb6a683cf2ce26b280358034bfe01cc345ab13bd
Zero Ex <0xb4dc0d3x@gmail.com>
2023-02-04 19:49:39 +0330

    add two query for PUT request

--- src/main/java/com/uni/register/Repositories/studentRepository.java
+++ src/main/java/com/uni/register/Repositories/studentRepository.java
@@ -9,6 +9,11 @@ import java.util.Optional;
 
 @Repository
 public interface studentRepository extends JpaRepository<student, Long> {
+    @Query("SELECT s FROM student s WHERE s.id = ?1")
+    Optional<student> findById(Long id);
+
+    @Query("SELECT s from student s where s.phoneNumber = ?1")
+    Optional<student> findByPhoneNumber(Long phonenumber);
 
     @Query("SELECT s FROM student s WHERE s.email = ?1")
     Optional<student> findByEmail(String email);
