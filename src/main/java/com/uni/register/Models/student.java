c03083cfbf422efc21cb1e4673b3edbea118bdcb
Zero Ex <0xb4dc0d3x@gmail.com>
2023-02-04 20:05:13 +0330

    Generated value

--- src/main/java/com/uni/register/Models/student.java
+++ src/main/java/com/uni/register/Models/student.java
@@ -8,16 +8,7 @@ import java.time.Period;
 @Table
 public class student {
     @Id
-    @SequenceGenerator(
-            name = "student_sequence",
-            sequenceName = "student_sequence",
-            allocationSize = 1
-    )
-    @GeneratedValue(
-            strategy = GenerationType.SEQUENCE,
-            generator = "student_sequence"
-    )
-
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
     private String email;
