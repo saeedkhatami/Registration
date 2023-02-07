fded0ee56e84200895c28d678c4268b8d68059b1
Zero Ex <0xb4dc0d3x@gmail.com>
2023-02-08 00:27:31 +0330

    adding role for security

---
+++ src/main/java/com/uni/register/Models/Role.java
@@ -0,0 +1,27 @@
+package com.uni.register.Models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="role")
+public class Role {
+    @Id
+    private Long id;
+    private String roleName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+}
