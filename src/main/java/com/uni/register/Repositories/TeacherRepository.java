f491b92622f43124bfa73b7179dac4756e26de3e
Zero Ex <0xb4dc0d3x@gmail.com>
2023-02-13 10:01:27 +0330

    add sorting on teacher repo

--- src/main/java/com/uni/register/Repositories/TeacherRepository.java
+++ src/main/java/com/uni/register/Repositories/TeacherRepository.java
@@ -1,14 +1,28 @@
 package com.uni.register.Repositories;
 
 import com.uni.register.Models.Teacher;
+import com.uni.register.Repositories.CustomRepository.CustomTeacherRepository;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface TeacherRepository extends JpaRepository<Teacher, Long> {
+public interface TeacherRepository extends JpaRepository<Teacher, Long>, CustomTeacherRepository {
+    Teacher findFirstByOrderByAgeAsc();
+
+    Teacher findTopByOrderByAgeAsc();
+
+    List<Teacher> findByOrderByAgeAsc();
+
+    List<Teacher> findByFirstNameIgnoreCase(String firstName);
+
+    List<Teacher> findByLastNameOrderByAgeAsc(String lastName);
+
+    List<Teacher> findByLastName(String lastName, Sort sort);
 
     @Query("SELECT s FROM Teacher s WHERE s.id = ?1")
     Optional<Teacher> findById(Long id);
