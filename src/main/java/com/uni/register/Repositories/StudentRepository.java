b3539bf7eff2e77984ee779488cf154ad77a9dc8
Zero Ex <0xb4dc0d3x@gmail.com>
2023-02-13 10:01:17 +0330

    add sorting on student repo

--- src/main/java/com/uni/register/Repositories/StudentRepository.java
+++ src/main/java/com/uni/register/Repositories/StudentRepository.java
@@ -1,14 +1,28 @@
 package com.uni.register.Repositories;
 
 import com.uni.register.Models.Student;
+import com.uni.register.Repositories.CustomRepository.CustomStudentRepository;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface StudentRepository extends JpaRepository<Student, Long> {
+public interface StudentRepository extends JpaRepository<Student, Long>, CustomStudentRepository {
+    Student findFirstByOrderByAgeAsc();
+
+    Student findTopByOrderByAgeAsc();
+
+    List<Student> findByOrderByAgeAsc();
+
+    List<Student> findByFirstNameIgnoreCase(String firstName);
+
+    List<Student> findByLastNameOrderByAgeAsc(String lastName);
+
+    List<Student> findByLastName(String lastName, Sort sort);
 
     @Query("SELECT s FROM Student s WHERE s.id = ?1")
     Optional<Student> findById(Long id);
