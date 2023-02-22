5f62469e275ced0db195e5cc6e48fe3740bb7abd
xbadcodex <0xb4dc0d3x@gmail.com>
2023-02-22 20:54:13 +0330

    I don't know what's going on

--- src/main/java/com/uni/register/Repositories/CustomRepository/CustomStudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.uni.register.Repositories.CustomRepository;
-
-import com.uni.register.Models.Student;
-
-import java.util.List;
-
-public interface CustomStudentRepository {
-    List<Student> findOrderedByAgeLimitedTo(Integer limit);
-}
